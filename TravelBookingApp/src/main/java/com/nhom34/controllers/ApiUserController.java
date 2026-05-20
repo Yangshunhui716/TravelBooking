29f1ad1a4e329e052f5726eeb9333d74e7683b38
Yangshunhui716 <thieuhuy856@gmail.com>
2026-05-20 18:59:19 +0700

    fix api login and api for provider

--- TravelBookingApp/src/main/java/com/nhom34/controllers/ApiUserController.java
+++ TravelBookingApp/src/main/java/com/nhom34/controllers/ApiUserController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ApiUserController {
     @Autowired
     private UserService userService;
