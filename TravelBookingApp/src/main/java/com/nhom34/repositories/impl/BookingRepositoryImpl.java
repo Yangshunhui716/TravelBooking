48e390e0ffba1d1dd26a0d47fe3f2e76d5a59d80
Yangshunhui716 <thieuhuy856@gmail.com>
2026-05-26 11:44:39 +0700

    fix merge from main

--- TravelBookingApp/src/main/java/com/nhom34/repositories/impl/BookingRepositoryImpl.java
+++ TravelBookingApp/src/main/java/com/nhom34/repositories/impl/BookingRepositoryImpl.java
@@ -5,8 +5,12 @@
 package com.nhom34.repositories.impl;
 
 import com.nhom34.pojo.Bookings;
+import com.nhom34.pojo.BookingsServiceDetail;
 import com.nhom34.repositories.BookingRepository;
 import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,4 +52,51 @@ public class BookingRepositoryImpl implements BookingRepository{
         return (Bookings) q.getSingleResult();
     }
     
+    @Override
+    public Bookings addBooking(Bookings booking) {
+        Session s = this.factory.getObject().getCurrentSession();
+        s.persist(booking);
+        
+        return booking;
+    }
+
+    @Override
+    public void addBookingDetail(List<BookingsServiceDetail> bookingDetail, Bookings booking) {
+        Session s = this.factory.getObject().getCurrentSession();
+        for (var b : bookingDetail) {
+            b.setBookingId(booking);
+            s.persist(b);
+        } 
+    }
+
+    @Override
+    public List<BookingsServiceDetail> getBookingDetailByBookingId(Long id) {
+        Session s = this.factory.getObject().getCurrentSession();
+        CriteriaBuilder builder = s.getCriteriaBuilder();
+        CriteriaQuery<BookingsServiceDetail> query = builder.createQuery(BookingsServiceDetail.class);
+        Root rBD = query.from(BookingsServiceDetail.class);
+        query.where(builder.equal(rBD.get("bookingId").get("id"), id));
+        Query q = s.createQuery(query);
+        return q.getResultList();
+    }
+
+    @Override
+    public void changePaymentStatus(Long id, String paymentStatus) {
+        Session s = this.factory.getObject().getCurrentSession();
+        Bookings b = this.getBookingById(id);
+        if (!b.getPaymentStatus().equals(paymentStatus)){
+            b.setPaymentStatus(paymentStatus);
+            s.merge(b);
+        }
+    }
+    
+    @Override
+    public void changeBookingStatus(Long id, String bookingStatus) {
+        Session s = this.factory.getObject().getCurrentSession();
+        Bookings b = this.getBookingById(id);
+        if (!b.getBookingStatus().equals(bookingStatus)){
+            b.setBookingStatus(bookingStatus);
+            s.merge(b);
+        }
+    }
 }
